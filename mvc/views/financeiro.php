59ff6506af248fd51a844b65d07d0b2b0916d6e4
moafsa <moafsa@gmail.com>
2025-11-04 16:42:38 -0300

    FIX: Adicionar jQuery na pagina financeiro.php - Exclusao falhava sem jQuery

--- mvc/views/financeiro.php
+++ mvc/views/financeiro.php
@@ -875,6 +875,7 @@ $contas = $db->fetchAll(
     </div>
 
     <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
